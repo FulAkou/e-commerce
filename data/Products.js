0d99afcac2d12c90b27681f1c818e067ff01bdbd
fulgenceBeugre <fulgencebeugre49@gmail.com>
2025-03-25 11:58:42 +0000

    ajout des produits et des utilisteurs dans la bd

--- data/Products.js
+++ data/Products.js
@@ -1,7 +1,6 @@
-const produits = [
+const products = [
   {
-    _id: "1",
-    name: "iPhone 9",
+    name: "iPhone 952",
     image:
       "https://www.google.com/imgres?q=iPhone%209&imgurl=https%3A%2F%2Fakm-img-a-in.tosshub.com%2Findiatoday%2Fimages%2Fstory%2F202001%2FUntitled-9.jpeg%3Fsize%3D1200%3A675&imgrefurl=https%3A%2F%2Fwww.indiatoday.in%2Ftechnology%2Fnews%2Fstory%2Fiphone-9-could-copy-iphone-11-camera-design-but-don-t-expect-dual-cameras-1639760-2020-01-24&docid=iSCped88IsDcpM&tbnid=-oCzAGkswLThGM&vet=12ahUKEwj3sr6cgKWMAxV4XEEAHYSMOpgQM3oECGgQAA..i&w=1200&h=675&hcb=2&ved=2ahUKEwj3sr6cgKWMAxV4XEEAHYSMOpgQM3oECGgQAA",
     description:
@@ -13,8 +12,7 @@ const produits = [
     numReviews: 8,
   },
   {
-    _id: "2",
-    name: "iPhone 9",
+    name: "iPhone 952",
     image:
       "https://www.google.com/imgres?q=iPhone%209&imgurl=https%3A%2F%2Fakm-img-a-in.tosshub.com%2Findiatoday%2Fimages%2Fstory%2F202001%2FUntitled-9.jpeg%3Fsize%3D1200%3A675&imgrefurl=https%3A%2F%2Fwww.indiatoday.in%2Ftechnology%2Fnews%2Fstory%2Fiphone-9-could-copy-iphone-11-camera-design-but-don-t-expect-dual-cameras-1639760-2020-01-24&docid=iSCped88IsDcpM&tbnid=-oCzAGkswLThGM&vet=12ahUKEwj3sr6cgKWMAxV4XEEAHYSMOpgQM3oECGgQAA..i&w=1200&h=675&hcb=2&ved=2ahUKEwj3sr6cgKWMAxV4XEEAHYSMOpgQM3oECGgQAA",
     description:
@@ -26,8 +24,7 @@ const produits = [
     numReviews: 8,
   },
   {
-    _id: "3",
-    name: "iPhone 9",
+    name: "iPhone 559",
     image:
       "https://www.google.com/imgres?q=iPhone%209&imgurl=https%3A%2F%2Fakm-img-a-in.tosshub.com%2Findiatoday%2Fimages%2Fstory%2F202001%2FUntitled-9.jpeg%3Fsize%3D1200%3A675&imgrefurl=https%3A%2F%2Fwww.indiatoday.in%2Ftechnology%2Fnews%2Fstory%2Fiphone-9-could-copy-iphone-11-camera-design-but-don-t-expect-dual-cameras-1639760-2020-01-24&docid=iSCped88IsDcpM&tbnid=-oCzAGkswLThGM&vet=12ahUKEwj3sr6cgKWMAxV4XEEAHYSMOpgQM3oECGgQAA..i&w=1200&h=675&hcb=2&ved=2ahUKEwj3sr6cgKWMAxV4XEEAHYSMOpgQM3oECGgQAA",
     description:
@@ -39,8 +36,7 @@ const produits = [
     numReviews: 8,
   },
   {
-    _id: "4",
-    name: "iPhone 9",
+    name: "iPhone 953",
     image:
       "https://www.google.com/imgres?q=iPhone%209&imgurl=https%3A%2F%2Fakm-img-a-in.tosshub.com%2Findiatoday%2Fimages%2Fstory%2F202001%2FUntitled-9.jpeg%3Fsize%3D1200%3A675&imgrefurl=https%3A%2F%2Fwww.indiatoday.in%2Ftechnology%2Fnews%2Fstory%2Fiphone-9-could-copy-iphone-11-camera-design-but-don-t-expect-dual-cameras-1639760-2020-01-24&docid=iSCped88IsDcpM&tbnid=-oCzAGkswLThGM&vet=12ahUKEwj3sr6cgKWMAxV4XEEAHYSMOpgQM3oECGgQAA..i&w=1200&h=675&hcb=2&ved=2ahUKEwj3sr6cgKWMAxV4XEEAHYSMOpgQM3oECGgQAA",
     description:
@@ -53,4 +49,4 @@ const produits = [
   },
 ];
 
-module.exports = produits;
+module.exports = products;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const mongoDBconnexion = require("./db-connexion/connectToDb");
+const databaseSeeder = require("./databaseSeeder");
 const app = express();
 PORT = process.env.PORT || 9000;
 
 app.use(express.json());
+
+app.use("/api/seed", databaseSeeder);
 
 mongoDBconnexion();
 app.listen(process.env.PORT, () => {
