90b08ec92471312684e2e3a25f695ce28ca19d0d
fulgenceBeugre <fulgencebeugre49@gmail.com>
2025-03-26 11:27:13 +0000

    add user profile

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const AsyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const generateToken = require("../utils/generateToken"); // Ajoute une fonction pour générer un token
+const { protect } = require("../middlewares/Auth");
 
 const userRouter = express.Router();
 
+//Login User
 userRouter.post(
   "/login",
   AsyncHandler(async (req, res) => {
@@ -46,5 +48,54 @@ userRouter.post(
   })
 );
 
-//create a user
+//create User
+userRouter.post(
+  "/",
+  AsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      res.status(400);
+      throw new Error("User already exist");
+    }
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+        token: generateToken(user._id), // Génération de token safegarder
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  })
+);
+
+//get user profile
+userRouter.get(
+  "/profile",
+  protect,
+  AsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
+    }
+  })
+);
 module.exports = userRouter;
